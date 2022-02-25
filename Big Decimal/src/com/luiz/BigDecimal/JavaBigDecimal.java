a2b0370f35417fc5a5e31b3b2844edf42952eb40
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-02-25 07:58:04 -0300

    Big Decimal

---
+++ Big Decimal/src/com/luiz/BigDecimal/JavaBigDecimal.java
@@ -0,0 +1,34 @@
+package com.luiz.BigDecimal;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class JavaBigDecimal {
+
+	public static void main(String[] args) {
+
+		double a = 0.03;
+		double b = 0.04;
+		double c = b - a;
+		System.out.println(c);
+
+		BigDecimal _a = new BigDecimal("0.03");
+		BigDecimal _b = new BigDecimal("0.04");
+		BigDecimal _c = _b.subtract(_a);
+		System.out.println(_c);
+
+		BigDecimal bd1 = new BigDecimal("123.456");
+		BigDecimal bd2 = new BigDecimal("987.654");
+		System.out.println(bd1.add(bd2));
+
+		System.out.println(bd1.multiply(bd2));
+
+		System.out.println(bd1.divide(new BigDecimal(2)));
+
+		BigInteger bi = new BigInteger("10000000000000000000");
+		System.out.println(bi);
+
+	}
+
+
+}
